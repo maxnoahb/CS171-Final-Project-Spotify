d2d7a22ce41155b8ba039d1f45ca4b05179c76a4
jfgonzalez99 <justingonzalez@college.harvard.edu>
2018-11-18 13:41:11 -0500

    imported data into main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 /* main JS file */
 
-console.log("Hello JS world!");
+// Global variables for data
+var audioData;
+var frequencyData;
+var mapData;
+
+d3.queue()
+    .defer(d3.csv, 'data/audio_features.csv')
+    .defer(d3.csv, 'data/track_frequencies.csv')
+    .defer(d3.json, 'data/world-110m.json')
+    .await(function(error, data1, data2, data3) {
+        console.log(data1, data2, data3);
+        audioData = data1;
+        frequencyData = data2;
+        mapData = data3;
+    });
