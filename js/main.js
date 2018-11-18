a33822e6cc1e95081419757edc021dd6e622d2a9
jfgonzalez99 <justingonzalez@college.harvard.edu>
2018-11-18 14:18:02 -0500

    changed numStrings to numbers

--- js/main.js
+++ js/main.js
@@ -11,11 +11,30 @@ d3.queue()
     .defer(d3.csv, 'data/track_frequencies.csv')
     .defer(d3.json, 'data/world-110m.json')
     .await(function(error, data1, data2, data3) {
-        console.log(data1, data2, data3);
+
+        // Convert numStrings to numbers
+        data1.forEach(function(d) {
+          d.acousticness = +d.acousticness;
+          d.danceability = +d.danceability;
+          d.duration_ms = +d.duration_ms;
+          d.energy = +d.energy;
+          d.instrumentalness = +d.instrumentalness;
+          d.liveness = +d.liveness;
+          d.loudness = +d.loudness;
+          d.speechiness = +d.speechiness;
+          d.tempo = +d.tempo;
+          d.valence = +d.valence;
+        });
+
+        data2.forEach(function(d) {
+          d.Freq = +d.Freq;
+        });
 
         audioData = data1;
         frequencyData = data2;
         mapData = data3;
+
+        console.log(audioData, frequencyData, mapData);
 
         // Initial bubble chart
         initializeBubbles();
