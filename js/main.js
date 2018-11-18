3c6583bfa02d71f42f7eec9d43e4d0641014971f
jfgonzalez99 <justingonzalez@college.harvard.edu>
2018-11-18 16:34:49 -0500

    created global variable for country data

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 var audioData;
 var frequencyData;
 var mapData;
+var dataByCountry;
 
 var comparisonChart;
 
@@ -36,7 +37,12 @@ d3.queue()
         frequencyData = data2;
         mapData = data3;
 
-        console.log(audioData, frequencyData, mapData);
+        dataByCountry = d3.nest()
+            .key(function(d) { return d.playlist_name.split(" ")[0]; })
+            .rollup(function (d) { return d; })
+            .entries(audioData);
+
+        console.log(audioData, frequencyData, mapData, dataByCountry);
 
         // Initial bubble chart
         initializeBubbles();
