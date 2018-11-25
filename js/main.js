a650787b5474c3bd7cea2600167cbc0b7eba4ec7
maxnoahb <maxnoahb@gmail.com>
2018-11-25 16:39:10 -0500

    added pagination scrolling starter code (using fullpage.js)

--- js/main.js
+++ js/main.js
@@ -16,94 +16,150 @@ var sliderVis;
 var choroplethMap;
 var introMap;
 
-// Load in data
-d3.queue()
-    .defer(d3.csv, 'data/audio_features.csv')
-    .defer(d3.csv, 'data/track_frequencies.csv')
-    .defer(d3.json, 'data/world-110m.json')
-    .defer(d3.tsv, 'data/world-110m-names.tsv')
-    .await(function(error, data1, data2, data3, data4) {
 
-        // Convert numStrings to numbers
-        data1.forEach(function(d) {
-          d.acousticness = +d.acousticness;
-          d.danceability = +d.danceability;
-          d.duration_ms = +d.duration_ms;
-          d.energy = +d.energy;
-          d.instrumentalness = +d.instrumentalness;
-          d.liveness = +d.liveness;
-          d.loudness = +d.loudness;
-          d.speechiness = +d.speechiness;
-          d.tempo = +d.tempo;
-          d.valence = +d.valence;
+
+$(document).ready(function() {
+    // Load in data
+    d3.queue()
+        .defer(d3.csv, 'data/audio_features.csv')
+        .defer(d3.csv, 'data/track_frequencies.csv')
+        .defer(d3.json, 'data/world-110m.json')
+        .defer(d3.tsv, 'data/world-110m-names.tsv')
+        .await(function(error, data1, data2, data3, data4) {
+
+            // Convert numStrings to numbers
+            data1.forEach(function(d) {
+                d.acousticness = +d.acousticness;
+                d.danceability = +d.danceability;
+                d.duration_ms = +d.duration_ms;
+                d.energy = +d.energy;
+                d.instrumentalness = +d.instrumentalness;
+                d.liveness = +d.liveness;
+                d.loudness = +d.loudness;
+                d.speechiness = +d.speechiness;
+                d.tempo = +d.tempo;
+                d.valence = +d.valence;
+            });
+
+            data2.forEach(function(d) {
+                d.Freq = +d.Freq;
+            });
+
+            audioData = data1;
+            frequencyData = data2;
+            mapData = data3;
+            mapNameData = data4;
+
+            // Organize data by countries
+            nestedByCountry = d3.nest()
+                .key(function(d, i) { return d.playlist_name.replace(" Top 50", ""); })
+                .rollup(function (d) { return d; })
+                .entries(audioData);
+
+            nestedByCountry.forEach(function(d) {
+                dataByCountry[d.key] = d.value;
+            });
+
+            audioData.forEach(function(d) {
+                if (uniqueSongData[d.track_name] == null) {
+                    uniqueSongData[d.track_name] = d;
+                }
+            });
+
+            // new data structure containing the average attributes for each country playlist
+            countryAvgAttributes = d3.nest()
+                .key(function(d) { return d.playlist_name; })
+                .rollup(function(v) {
+                    return {
+                        "acousticness": d3.mean(v, function(d) { return d.acousticness; }),
+                        "danceability": d3.mean(v, function(d) { return d.danceability; }),
+                        "duration_ms": d3.mean(v, function(d) { return d.duration_ms; }),
+                        "energy": d3.mean(v, function(d) { return d.energy; }),
+                        "liveness": d3.mean(v, function(d) { return d.liveness; }),
+                        "loudness": d3.mean(v, function(d) { return d.loudness; }),
+                        "speechiness": d3.mean(v, function(d) { return d.speechiness; }),
+                        "tempo": d3.mean(v, function(d) { return d.tempo; }),
+                        "valence": d3.mean(v, function(d) { return d.valence; })
+                    };
+                })
+                .entries(data1);
+
+            console.log(countryAvgAttributes);
+
+            // console.log(audioData, frequencyData, mapData, dataByCountry, data4);
+            // console.log(uniqueSongData);
+
+            // Initialize intro map
+            introMap = new IntroMap("intro-map", countryAvgAttributes, data3, data4);
+
+            // Initialize comparison chart
+            comparisonChart = new ComparisonChart("comparison-chart", audioData);
+
+            // Initial bubble chart
+            // bubbleScale();
+            updateBubbles("United States");
+
+            sliderVis = new SliderVis("slider-chart", countryAvgAttributes, data3, data4);
+
+            choroplethMap = new ChoroplethVis("choropleth-map", countryAvgAttributes, data3, data4)
+
         });
 
-        data2.forEach(function(d) {
-          d.Freq = +d.Freq;
-        });
+    function updateSelected() {
+        selectedCountry = d3.select("#countries-list").property("value");
+        comparisonChart.onCountryCompareChange();
+        // updateBubbles(selectedCountry);
+    }
 
-        audioData = data1;
-        frequencyData = data2;
-        mapData = data3;
-        mapNameData = data4;
+    /*$('#fullpage').fullpage({
+        sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+        sectionSelector: '.vertical-scrolling',
+        // slideSelector: '.horizontal-scrolling',
+        navigation: true,
+        // slidesNavigation: true,
+        css3: true,
+        controlArrows: false,
+        anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+        // menu: '#menu',
 
-        // Organize data by countries
-        nestedByCountry = d3.nest()
-            .key(function(d, i) { return d.playlist_name.replace(" Top 50", ""); })
-            .rollup(function (d) { return d; })
-            .entries(audioData);
+        afterLoad: function (anchorLink, index) {
+            $header_top.css('background', 'rgba(0, 47, 77, .3)');
+            $nav.css('background', 'rgba(0, 47, 77, .25)');
+            if (index == 5) {
+                $('#fp-nav').hide();
+            }
+        },
 
-        nestedByCountry.forEach(function(d) {
-          dataByCountry[d.key] = d.value;
-        });
+        onLeave: function (index, nextIndex, direction) {
+            if (index == 5) {
+                $('#fp-nav').show();
+            }
+        },
 
-        audioData.forEach(function(d) {
-          if (uniqueSongData[d.track_name] == null) {
-            uniqueSongData[d.track_name] = d;
-          }
-        });
+        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+            if (anchorLink == 'fifthSection' && slideIndex == 1) {
+                $.fn.fullpage.setAllowScrolling(false, 'up');
+                $header_top.css('background', 'transparent');
+                $nav.css('background', 'transparent');
+                $(this).css('background', '#374140');
+                $(this).find('h2').css('color', 'white');
+                $(this).find('h3').css('color', 'white');
+                $(this).find('p').css(
+                    {
+                        'color': '#DC3522',
+                        'opacity': 1,
+                        'transform': 'translateY(0)'
+                    }
+                );
+            }
+        },
 
-        // new data structure containing the average attributes for each country playlist
-        countryAvgAttributes = d3.nest()
-            .key(function(d) { return d.playlist_name; })
-            .rollup(function(v) {
-                return {
-                    "acousticness": d3.mean(v, function(d) { return d.acousticness; }),
-                    "danceability": d3.mean(v, function(d) { return d.danceability; }),
-                    "duration_ms": d3.mean(v, function(d) { return d.duration_ms; }),
-                    "energy": d3.mean(v, function(d) { return d.energy; }),
-                    "liveness": d3.mean(v, function(d) { return d.liveness; }),
-                    "loudness": d3.mean(v, function(d) { return d.loudness; }),
-                    "speechiness": d3.mean(v, function(d) { return d.speechiness; }),
-                    "tempo": d3.mean(v, function(d) { return d.tempo; }),
-                    "valence": d3.mean(v, function(d) { return d.valence; })
-                };
-            })
-            .entries(data1);
-
-        console.log(countryAvgAttributes);
-
-        // console.log(audioData, frequencyData, mapData, dataByCountry, data4);
-        // console.log(uniqueSongData);
-
-        // Initialize intro map
-        introMap = new IntroMap("intro-map", countryAvgAttributes, data3, data4);
-
-        // Initialize comparison chart
-        comparisonChart = new ComparisonChart("comparison-chart", audioData);
-
-        // Initial bubble chart
-        // bubbleScale();
-        updateBubbles("United States");
-
-        sliderVis = new SliderVis("slider-chart", countryAvgAttributes, data3, data4);
-
-        choroplethMap = new ChoroplethVis("choropleth-map", countryAvgAttributes, data3, data4)
-
-    });
-
-function updateSelected() {
-  selectedCountry = d3.select("#countries-list").property("value");
-  comparisonChart.onCountryCompareChange();
-  // updateBubbles(selectedCountry);
-}
+        onSlideLeave: function (anchorLink, index, slideIndex, direction) {
+            if (anchorLink == 'fifthSection' && slideIndex == 1) {
+                $.fn.fullpage.setAllowScrolling(true, 'up');
+                $header_top.css('background', 'rgba(0, 47, 77, .3)');
+                $nav.css('background', 'rgba(0, 47, 77, .25)');
+            }
+        }
+    });*/
+});
